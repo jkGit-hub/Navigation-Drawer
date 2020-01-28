fe60f026dc7cb30052ffdb27c6b116da76d28e45
jkGit <jk1013@hotmail.com>
2020-01-28 14:04:08 -0800

    Navigation Drawer

---
+++ app/src/main/java/com/jkapps/navdrawertest/homeFragment.java
@@ -0,0 +1,17 @@
+package com.jkapps.navdrawertest;
+
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import androidx.fragment.app.Fragment;
+
+public class homeFragment extends Fragment {
+    View view;
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
